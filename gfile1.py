79056fc2b8741f2cbb93f03b487d2498ac203b4c
Gaven Neufeld <gavensmac@Gavens-MacBook-Pro.local>
2018-03-16 11:35:17 -0600

    I added stuff

--- gfile1.py
+++ gfile1.py
@@ -1,1 +1,6 @@
 print("This is where Gaven wrote stuff")
+
+print("I added a ton of stuff")
+
+def itll_bealright():
+    print("Don't Worry")
--- jfile2.py
+++ jfile2.py
@@ -1,1 +1,3 @@
 print("This is where Jake put his stuff")
+
+print("Gaven was here")
